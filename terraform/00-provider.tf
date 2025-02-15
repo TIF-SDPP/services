38fca22a30c50b3f9c35feae8dcf23a8797c1762
leoduville <71992403+leoduville@users.noreply.github.com>
2025-02-14 21:36:51 -0300

    tt

--- terraform/00-provider.tf
+++ terraform/00-provider.tf
@@ -1,15 +1,16 @@
-
 terraform {
+
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = ">=5.26.0"
     }
-  }
-  backend "gcs" {
-  }
 
-  required_version = ">= 1.7.5"
+  }
+  #backend "gcs" {
+  #}
+
+  #required_version = ">= 1.7.5"
 }
 
 provider "google" {
