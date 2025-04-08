4d3f4d08f7c395ea15e47231859562e3cba2e3d8
leoduville <71992403+leoduville@users.noreply.github.com>
2025-04-07 23:02:06 -0300

    actualizo

--- script2.sh
+++ script2.sh
@@ -3,6 +3,7 @@
 # Script para matar todos los Services
 kubectl delete service public-service-coordinador
 kubectl delete service public-service-poolmanager
+kubectl delete service public-service-rabbitmq
 kubectl delete service service-coordinador
 kubectl delete service service-poolmanager
 kubectl delete service service-rabbitmq
