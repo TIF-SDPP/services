abf46fdce491960c3fb1776f786e5019e4662a99
leoduville <71992403+leoduville@users.noreply.github.com>
2025-05-13 01:16:12 -0300

    actualizo

--- script2.sh
+++ script2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Script para matar todos los Services
-kubectl delete service public-service-coordinador
-kubectl delete service public-service-poolmanager
-kubectl delete service public-service-rabbitmq
+#kubectl delete service public-service-coordinador
+#kubectl delete service public-service-poolmanager
+#kubectl delete service public-service-rabbitmq
 kubectl delete service service-coordinador
 kubectl delete service service-poolmanager
 kubectl delete service service-rabbitmq
 kubectl delete service service-redis
-kubectl delete service service-worker
+#kubectl delete service service-worker
 kubectl delete service server-app
